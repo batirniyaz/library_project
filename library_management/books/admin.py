3b1c2e06f06d68f3488da80f06c29e3a4d4df5d9
Batir Muratbaev <muratbaevbatirniyaz@gmail.com>
2024-04-17 00:30:49 +0500

    Customize admin panel

--- library_management/books/admin.py
+++ library_management/books/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Book
 
-# Register your models here.
 
-admin.site.register(Book)
+# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'subtitle', 'author', 'isbn', 'price')
+    search_fields = ('title', 'subtitle', 'author', 'isbn', 'price')
+    list_filter = ('title', 'subtitle', 'author', 'isbn', 'price')
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.site_header = "Library Management System"
+admin.site.site_title = "Library Management System"
+admin.site.index_title = "Welcome to Library Management System"
