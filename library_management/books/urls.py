f4684948a0ba0edcdc02facfa479ab85260dcba4
Batir Muratbaev <muratbaevbatirniyaz@gmail.com>
2024-04-17 00:33:33 +0500

    Add four paths: CRUD in urls.py

--- library_management/books/urls.py
+++ library_management/books/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import BookListApiView, book_list_view
+from .views import BookListApiView, book_list_view, BookDetailApiView, BookDeleteApiView, BookUpdateApiView, BookCreateApiView
 
 urlpatterns = [
     path('', BookListApiView.as_view()),
-    path("book/", book_list_view)
+    path("books/create/", BookCreateApiView.as_view()),
+    path("books/<int:pk>/", BookDetailApiView.as_view()),
+    path("books/<int:pk>/delete/", BookDeleteApiView.as_view()),
+    path("books/<int:pk>/update/", BookUpdateApiView.as_view()),
+    path("book/", book_list_view),
 ]
