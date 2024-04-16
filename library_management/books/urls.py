705528d5f2403a7747b2d9e175e52cfa320a67d8
Batir Muratbaev <muratbaevbatirniyaz@gmail.com>
2024-04-17 01:58:47 +0500

    Add to paths: booklistcreate, bookupdatedelete to urls.py

--- library_management/books/urls.py
+++ library_management/books/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import BookListApiView, book_list_view, BookDetailApiView, BookDeleteApiView, BookUpdateApiView, BookCreateApiView
+from .views import BookListApiView, BookDetailApiView, BookDeleteApiView, BookUpdateApiView, BookCreateApiView, BookListCreateApiView, BookUpdateDeleteApiView
 
 urlpatterns = [
-    path('', BookListApiView.as_view()),
+    path('books/', BookListApiView.as_view()),
+    path('booklistcreate/', BookListCreateApiView.as_view()),
+    path('bookupdatedelete/<int:pk>/', BookUpdateDeleteApiView.as_view()),
     path("books/create/", BookCreateApiView.as_view()),
     path("books/<int:pk>/", BookDetailApiView.as_view()),
     path("books/<int:pk>/delete/", BookDeleteApiView.as_view()),
     path("books/<int:pk>/update/", BookUpdateApiView.as_view()),
-    path("book/", book_list_view),
 ]
