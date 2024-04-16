a916d8f021fcdb82486f42bb0facf49981ec2ab6
Batir Muratbaev <muratbaevbatirniyaz@gmail.com>
2024-04-17 00:31:20 +0500

    Add content field to models.py

--- library_management/books/models.py
+++ library_management/books/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     subtitle = models.CharField(max_length=200)
+    content = models.TextField(default="")
     author = models.CharField(max_length=100)
     isbn = models.CharField(max_length=13)
     price = models.DecimalField(max_digits=20, decimal_places=2)
