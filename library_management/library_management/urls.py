3a1d40e1e1a39071af22da647d504822fd52ad44
Batir Muratbaev <muratbaevbatirniyaz@gmail.com>
2024-04-17 10:24:11 +0500

    Configure swagger and redoc in main urls.py

--- library_management/library_management/urls.py
+++ library_management/library_management/urls.py
@@ -16,8 +16,31 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Library Management API",
+        default_version="v1",
+        description="API for Library Management System",
+        terms_of_service="demo.com",
+        contact=openapi.Contact(email="muratbaevbatirniyaz@gmail.com"),
+        license=openapi.License(name="Demo License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny, ],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/v1/", include("books.urls")),
+
+    # Swagger and Redoc
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+
 ]
+
+
