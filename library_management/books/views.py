95b0f0f03cc20528e141100275c9eb3f84da580d
Batir Muratbaev <muratbaevbatirniyaz@gmail.com>
2024-04-17 14:36:08 +0500

    Change type of view from generics for Apiviews in views.py

--- library_management/books/views.py
+++ library_management/books/views.py
@@ -1,21 +1,55 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Book
 from .serializers import BookSerializer
 
-from rest_framework import generics
+from rest_framework import generics, status
 
 
 # Create your views here.
-class BookListApiView(generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+# class BookListApiView(generics.ListAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+
+class BookListApiView(APIView):
+    def get(self, request):
+        books = Book.objects.all()
+        serializer_data = BookSerializer(books, many=True).data
+        data = {
+            "status": "success",
+            "code": 200,
+            "message": f"Returned {len(books)} books",
+            "books": serializer_data,
+        }
+        return Response(data)
 
 
-class BookDetailApiView(generics.RetrieveAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+# class BookDetailApiView(generics.RetrieveAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+
+class BookDetailApiView(APIView):
+    def get(self, request, pk):
+        try:
+            book = Book.objects.get(id=pk)
+            serializer_data = BookSerializer(book).data
+
+            data = {
+                "status": "success",
+                "code": 200,
+                "message": "Book returned successfully",
+                "book": serializer_data,
+            }
+            return Response(data, status=status.HTTP_200_OK)
+        except Exception as e:
+            data = {
+                "status": "error",
+                "code": 404,
+                "message": "Book not found",
+            }
+            return Response(data, status=status.HTTP_404_NOT_FOUND)
 
 
 class BookDeleteApiView(generics.DestroyAPIView):
@@ -28,9 +62,40 @@ class BookUpdateApiView(generics.UpdateAPIView):
     serializer_class = BookSerializer
 
 
-class BookCreateApiView(generics.CreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+# class BookCreateApiView(generics.CreateAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+
+
+class BookCreateApiView(APIView):
+
+    def post(self, request):
+        try:
+            data = request.data
+            serializer = BookSerializer(data=data)
+            if serializer.is_valid():
+                serializer.save()
+                data = {
+                    "status": "success",
+                    "code": 201,
+                    "message": "Book created successfully",
+                    "book": serializer.data,
+                }
+                return Response(data)
+            else:
+                data = {
+                    "status": "error",
+                    "code": 400,
+                    "message": serializer.errors,
+                }
+                return Response(data, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            data = {
+                "status": "error",
+                "code": 400,
+                "message": str(e),
+            }
+            return Response(data, status=status.HTTP_400_BAD_REQUEST)
 
 
 class BookListCreateApiView(generics.ListCreateAPIView):
